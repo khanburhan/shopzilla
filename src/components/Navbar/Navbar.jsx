36dfe54d360c8aa0baa09e3814371bd36eb9fa64
bc0der11 <k.khanburhan@gmail.comgit config --global user.name Burhan>
2020-12-26 17:35:34 +0530

    upload all files to github

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.AppBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="ShopZilla"
+              height="25px"
+              className={classes.image}
+            />
+            ShopZilla
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
